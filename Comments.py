9d0e01eda39a3211358a8b2694b70c6852c2d7e2
Rinat <nicken1898@gmail.com>
2021-06-04 00:07:25 +0300

    Юнит тесты у Комментариев

--- Comments.py
+++ Comments.py
@@ -13,43 +13,52 @@ def Entry_form(): #присвоение переменным значения с
     Country = request.forms.get('country')
     Comment = request.forms.get('comment')
 
-    f=open ("Comments.txt","a") #Открытие файла
+    match = '[a-zA-Z0-9+-]+[@][a-zA-Z0-9-.]+[^.].[a-zA-Z0-9-.]{2,3}$'
+    if Surname=="" or Name=="" or Date=="" or email=="" or Comment=="":
+        return "Not all fields are filled "
+    else:
+         if (re.search(match, mail)):
+              f=open ("Comments.txt","a") #Открытие файла
 
-    f.writelines("------------------------------------------------------------------------------------------"+"\n") #Запись в файл
-    f.writelines("Surname: "+Surname+"\n")
-    f.writelines("Name: "+Name+"\n")
-    f.writelines("Date: "+Date+"\n")
-    f.writelines("email: "+email+"\n")
-    f.writelines("Gender: "+Gender+"\n")
-    f.writelines("Country: "+Country+"\n")
-    f.writelines("Comment: "+Comment+"\n")
+              f.writelines("------------------------------------------------------------------------------------------"+"\n") #Запись в файл
+              f.writelines("Surname: "+Surname+"\n")
+              f.writelines("Name: "+Name+"\n")
+              f.writelines("Date: "+Date+"\n")
+              f.writelines("email: "+email+"\n")
+              f.writelines("Gender: "+Gender+"\n")
+              f.writelines("Country: "+Country+"\n")
+              f.writelines("Comment: "+Comment+"\n")
+              f.close()
+                  #-----------------------------------Вывод кнопки Возвращения назад------------------------------------
+              return  ('''<h2 align=center>Review has been sent, to go back click on the button</h2>'''+"<br>"+''' 
+                <style>
 
-    
-    f.close()
+                    h2
+                    {
+                    font-size: 200%;
+                    font-family: Verdana, Arial, Helvetica, sans-serif;
+                    color: #808080;
+                    }
+                    .h4 {
+                    border-radius: 12px;
+                    background: wight;
+                    border-color: #cccccc;
+                    color: black;
+                    font-size: 9pt;
+                    height: 70px;
+                    width: 200px;
+                    position: relative;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%,0);
+                    }
+                </style>
+                    <p><a href="/Comments"><input class=h4  type="submit" value="Back"></a></p>''')
+           #-------------------------------------------------------------------------------------------------------
+         else:
+             return"invalid number"
 
-    #-----------------------------------Вывод кнопки Возвращения назад------------------------------------
-    return  ('''<h2 align=center>Review has been sent, to go back click on the button</h2>'''+"<br>"+''' 
-        <style>
 
-            h2
-            {
-            font-size: 200%;
-            font-family: Verdana, Arial, Helvetica, sans-serif;
-            color: #808080;
-            }
-            .h4 {
-            border-radius: 12px;
-            background: wight;
-            border-color: #cccccc;
-            color: black;
-            font-size: 9pt;
-            height: 70px;
-            width: 200px;
-            position: relative;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%,0);
-            }
-        </style>
-            <p><a href="/Comments"><input class=h4  type="submit" value="Back"></a></p>''')
-   #-------------------------------------------------------------------------------------------------------
+   
+
+
--- test_2.py
+++ test_2.py
@@ -1,14 +1,11 @@
 import unittest
 import re
-import myform
-class Test_test_2(unittest.TestCase):
+import Comments
+class Test_test_1(unittest.TestCase):
     def test_T(self):
-        regex=re.search(r'([@]mail[.]ru)',myform.my_form())
-        if (regex==None):
-            a=False
-        if (regex!=None):
-            a=True
-        self.assertTrue(a)
-
+        list_mail = ["m.m@mail.ru", "m1.@mail.com", "m1@gmail.ru", "regr@mail.com"]
+        for i in range (len(list_mail)):
+            regex=re.search(r'[a-zA-Z0-9+-]+[@][a-zA-Z0-9-.]+[^.].[a-zA-Z0-9-.]{2,3}$', list_mail[i])
+        self.assertTrue(regex)
 if __name__ == '__main__':
     unittest.main()
